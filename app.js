4bf5d21287b750c1c0dc2acefa8cf5e39de46cb8
Joy Serquiña <stepjoyous@gmail.com>
2023-01-16 12:18:30 -0800

    Created different paths and a form that posts a message to the server and saves it into a message.txt file successfully.

--- app.js
+++ app.js
@@ -1,7 +1,34 @@
 const http = require('http')
+const fs = require('fs')
+const { parse } = require('path')
 
 const server = http.createServer((req,res) => {
-  console.log(req.url, req.method, req.headers)
+  const { url, method } = {...req}
+  if (url === '/') {
+    res.setHeader('Content-Type', 'text/html')
+    res.write(`<html>`)
+    res.write(`<head><title>Enter Message</title></head>`)
+    res.write(`<body><form action='/message' method='POST'><input type='text' name='message'/><button type='submit'>Submit</button></form></body>`)
+    res.write(`</html>`)
+    return res.end()
+  }
+  if (url === '/message' && method ==='POST') {
+    const body = []
+    req.on('data', (chunk) => {
+      console.log(chunk)
+      body.push(chunk)
+    })
+    req.on('end', () => {
+      const parsedBody = Buffer.concat(body).toString()
+      console.log(parsedBody)
+      const message = parsedBody.split('=')[1]
+      fs.writeFileSync('message.txt', message)
+    })
+    
+    res.statusCode = 302
+    res.setHeader('Location', '/')
+    return res.end()
+  }
   res.setHeader('Content-Type', 'text/html')
   res.write(`<html>`)
   res.write(`<head><title>My First Page</title></head>`)
