7dc65635831a19a825912da7e5f4e6b9c0430f1f
Joy Serquiña <stepjoyous@gmail.com>
2023-01-16 22:28:17 -0800

    More notes for the middleware usage.

--- app.js
+++ app.js
@@ -3,6 +3,12 @@ const express = require('express')
 const app = express()
 
 // middleware
+app.use('/', (req, res, next) => {
+  console.log('This always runs!')
+  // if you are having middleware with a next, have it go ABOVE the routes
+  next()
+})
+
 // Note paths should have least complex (ie. '/') on bottom as it will filter
 // out as it goes down
 app.use('/add-product', (req,res,next) => {
