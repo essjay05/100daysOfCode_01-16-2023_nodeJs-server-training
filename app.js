a595836dbe525e504486e6e08c829d654665a871
Joy Serquiña <stepjoyous@gmail.com>
2023-01-17 12:07:16 -0800

    Created catchall for page not found and added status 404.

--- app.js
+++ app.js
@@ -16,6 +16,11 @@ app.use(bodyParser.urlencoded({ extended: false }))
 app.use(adminRoutes)
 app.use(shopRoutes)
 
+// 404 catchall
+app.use((req, res, next) => {
+  res.status(404).send(`<h1>Page not found</h1>`)
+})
+
 app.listen(PORT, () => {
   console.log(`App listening on PORT: ${PORT}`)
 })
