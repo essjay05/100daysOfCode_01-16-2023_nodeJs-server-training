f7ce40e86e7d2139717fdf98f94a8c7463c7c21b
Joy Serquiña <stepjoyous@gmail.com>
2023-01-17 14:22:36 -0800

    Updated routes to serve html files found in views and added basic styling to the pages via a style tag in the header.

--- routes/admin.js
+++ routes/admin.js
@@ -1,6 +1,8 @@
 const path = require('path')
+
 const express = require('express')
 
+const rootDir = require('../util/path')
 
 const router = express.Router()
 
@@ -13,7 +15,7 @@ const router = express.Router()
 
 // /admin/add-product => GET
 router.get('/add-product', (req,res,next) => {
-  res.sendFile(path.join(__dirname, '../', 'views', 'add-product.html'))
+  res.sendFile(path.join(rootDir, 'views', 'add-product.html'))
 })
 
 
