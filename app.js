f1b7706ff30c2bca4d8ddba4150222a378b3fdf0
Joy Serquiña <stepjoyous@gmail.com>
2023-01-18 14:32:25 -0800

    Removed all style tags in html files and moved them to external files in a public/css folder which we added middleware to our app.js to be able to access those external css files.

--- app.js
+++ app.js
@@ -11,7 +11,7 @@ const shopRoutes = require('./routes/shop')
 
 // middleware
 app.use(bodyParser.urlencoded({ extended: false }))
-
+app.use(express.static(path.join(__dirname, 'public')))
 
 // Routes
 app.use('/admin', adminRoutes)
