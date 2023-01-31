8b7301141c5243fb76df659d9cfba13dc2d36e83
Joy Serquiña <stepjoyous@gmail.com>
2023-01-30 21:53:55 -0800

    Created products array to save title input changes to successfully.

--- routes/admin.js
+++ routes/admin.js
@@ -6,6 +6,8 @@ const rootDir = require('../util/path')
 
 const router = express.Router()
 
+const products = []
+
 // Routes
 
 // Note: paths should have least complex (ie. '/') on bottom as it will filter
@@ -21,8 +23,10 @@ router.get('/add-product', (req,res,next) => {
 
 // /admin/add-product => POST
 router.post('/add-product', (req, res, next) => {
+  products.push({ title: req.body.title })
   console.log(req.body)
   res.redirect('/')
 })
 
-module.exports = router
+exports.routes = router;
+exports.products = products;
